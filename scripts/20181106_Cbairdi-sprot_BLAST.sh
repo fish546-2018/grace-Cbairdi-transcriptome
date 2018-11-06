f917062311ea70cf3176babc01c49de029eac768
grace-ac <graceac9@uw.edu>
2018-11-06 14:02:47 -0800

    upload

--- scripts/20181106_Cbairdi-sprot_BLAST.sh
+++ scripts/20181106_Cbairdi-sprot_BLAST.sh
@@ -8,26 +8,26 @@
 ## Nodes 
 #SBATCH --nodes=1   
 ## Walltime (days-hours:minutes:seconds format)
-#SBATCH --time=90:30:00
+#SBATCH --time=3-20:30:00
 ## Memory per node
-#SBATCH --mem=500G
+#SBATCH --mem=100G
 ##turn on e-mail notification
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=graceac9@uw.edu
 ## Specify the working directory for this job
-#SBATCH --workdir=/gscratch/srlab/graceac9/analyses
+#SBATCH --workdir=/gscratch/srlab/graceac9/analyses/1106-Cb-sprot-blast
 
 # Load Python Mox module for Python module availability
 
 module load intel-python3_2017
 
 
-/gscratch/srlab/programs/ \ #need to find where blast lives on mox
+/gscratch/srlab/programs/ncbi-blast-2.6.0+ \
 --blastn \
---query Trinity.fasta \ #OR do I have to rsync query.fa from owl/scaphapoda/grace/Crab-project/fasta_files/11012018_query_library01/query.fa
---db http://owl.fish.washington.edu/scaphapoda/grace/Crab-project/blastdb/uniprot_sprot \
+--query /gscratch/srlab/graceac9/query/library01/query.fa \
+--db /gscratch/srlab/graceac9/blastdb/uniprot_sprot/blastdb/*uniprot_sprot \
 --max_target_seqs 1 \
 --outfmt 6 \
---num_threads 8 \
+--num_threads 28 \
 --out 1106-cbairdiblast-sprot.tab
 --CPU 28
