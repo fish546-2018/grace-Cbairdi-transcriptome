6a6ddf4aac2c8fa727827c2b40e01d525dada14f
Grace Crandall <graceac9@uw.edu>
2018-11-13 13:51:34 -0800

    add to script

--- scripts/plots.R
+++ scripts/plots.R
@@ -1,6 +1,11 @@
 install.packages("plotly")
+library(dplyr)
+library(ggplot2)
+library(plotly)
 
 blastq <- read.delim("analyses/Blastquery-GOslim.sorted", header = FALSE, sep = "\t")
 
+  
+ggplot(blastq, aes(x = factor(1), fill=blastq$V2))+geom_bar(width = 1)+coord_polar("y")
 
 
