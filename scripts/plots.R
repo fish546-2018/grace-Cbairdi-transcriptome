7e7d9d21e14b0653599e1b4f3fe16f2dac3459c4
Grace Crandall <graceac9@uw.edu>
2018-11-29 15:40:15 -0800

    commit

--- scripts/plots.R
+++ scripts/plots.R
@@ -2,10 +2,15 @@ install.packages("plotly")
 library(dplyr)
 library(ggplot2)
 library(plotly)
+library(reshape2)
 
 blastq <- read.delim("analyses/Blastquery-GOslim.sorted", header = FALSE, sep = "\t")
 
 write.csv(blastq, "/analyses/Blastquery-GOslim-sep.csv")
+
+
+#make categories into columns, have contents be the number of trinity outputs in that category. make pie chart. 
+
   
 ggplot(blastq, aes(x = factor(1), fill=blastq$V2))+geom_bar(width = 1)+coord_polar("y")
 
