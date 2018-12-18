2099d0eab3f4356249efadfac426573f51781022
Grace Crandall <graceac9@uw.edu>
2018-12-18 11:32:37 -0800

    edit col names

--- scripts/taxa-crab-breakdown2.R
+++ scripts/taxa-crab-breakdown2.R
@@ -1,18 +1,16 @@
-#read in cg-trinity-nt.tab from http://gannet.fish.washington.edu/seashell/bu-mox/analyses/1114b/cg-trinity-nt.tab
+newtax <- read.table("http://owl.fish.washington.edu/scaphapoda/grace/Crab-project/1129-Cb-tax-blast.tab", sep = "\t", header = FALSE)
 
-tax <- read.table("http://gannet.fish.washington.edu/seashell/bu-mox/analyses/1114b/cg-trinity-nt.tab", sep = "\t", header = FALSE)
-
-colnames(tax)
+colnames(newtax)
 #[1] "V1" "V2" "V3" "V4" "V5" "V6" "V7" "V8"
 
 #need to add descriptive col names (still need to figure out what columns V2-V5 are... but for my visual I'm focusing on "genus_species" and "common_name")
-colnms <- c("Trinity_seq_ID", "Subject_seq_ID", "evalue","bitscore","subject_taxonomy", "genus_species","common_name", "kingdom")
-colnames(tax) <- colnms
+colnms <- c("Trinity_protein", "V2", "V3","V4","V5", "genus_species","common_name", "Domain")
+colnames(newtax) <- colnms
 
 
 #Make a pie chart to include the breakdown of the "crab-related" genus -es
 #create subset of just the "genus_species" and the "common_name" columns
-gsc <- subset(tax, select = c(genus_species, common_name))
+gsc <- subset(newtax, select = c(genus_species, common_name))
 
 #find unique species in "genus_species"
 uspec <- unique(gsc$genus_species)
@@ -31,7 +29,3 @@ chiono <- length(grep("Chionoecetes", gsc$genus_species))
 #find out how many of species names include "Chionoecetes"
 Chion <- length(grep("Chionoecetes", gsc$genus_species))
 #45 proteins are from Chionoecetes genus
-
-#find number of species that are associated with common names that include "crab"... how can i pull out a list of all the different crab species and make a pie chart of their relative proportions?
-
-
